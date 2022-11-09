4d572cdd491ce02e9abf7889acb3f6c99ffcac0e
chistvas <107794272+chistvas@users.noreply.github.com>
2022-11-09 15:59:09 +0300

    added migrate engine

--- application/__init__.py
+++ application/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SECRET_KEY'] = '395f645c16a60709237dd22e9845ec41'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from application import routes
